cbece39b9a9c5f5e084c7bbf7dcb3035c616b3e6
Nelmeris <kufaevartem@icloud.com>
2019-05-12 22:47:57 +0300

    Initial Commit

---
+++ YouLang-MobileTests/YouLang_MobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  YouLang_MobileTests.swift
+//  YouLang-MobileTests
+//
+//  Created by Artem Kufaev on 12/05/2019.
+//  Copyright © 2019 Artem Kufaev. All rights reserved.
+//
+
+import XCTest
+@testable import YouLang_Mobile
+
+class YouLang_MobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
